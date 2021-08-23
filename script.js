6bd57dd5f02ad90ef465cb7ecd163aa89e1fc93a
shellachigi <shellachigi@gmail.com>
2021-08-23 21:53:43 +0800

    add activity

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// let fname = prompt('Enter your first name');
+// let lname = prompt('Enter your last name');
+// console.log(fname + ' ' + lname);
+
+// let email = prompt('Enter your email');
+// console.log(email);
+
+// let number = prompt('Enter your number');
+// console.log(number);
+
+function printUserInfo(name, mail, num) {
+    let fname = prompt('Enter your first name');
+    let lname = prompt('Enter your last name');
+    console.log(fname + ' ' + lname);
+    let email = prompt('Enter your email');
+    console.log(email);
+    let number = prompt('Enter your number');
+    console.log(number);
+}
+printUserInfo('tony starks', 'ironman@gmail.com', '0917111111')
+
